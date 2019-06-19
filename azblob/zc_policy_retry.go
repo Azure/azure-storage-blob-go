539b05f2549a2ca975f305a038e7babc6c9655bf
Adele Reed <v-adreed@microsoft.com>
2019-06-19 10:39:55 -0700

    Fix a nil pointer dereference when retrying

--- azblob/zc_policy_retry.go
+++ azblob/zc_policy_retry.go
@@ -211,7 +211,7 @@ func NewRetryPolicyFactory(o RetryOptions) pipeline.Factory {
 				switch {
 				case ctx.Err() != nil:
 					action = "NoRetry: Op timeout"
-				case !tryingPrimary && response != nil && response.Response().StatusCode == http.StatusNotFound:
+				case !tryingPrimary && response != nil && response.Response() != nil && response.Response().StatusCode == http.StatusNotFound:
 					// If attempt was against the secondary & it returned a StatusNotFound (404), then
 					// the resource was not found. This may be due to replication delay. So, in this
 					// case, we'll never try the secondary again for this operation.
