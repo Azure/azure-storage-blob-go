11581b0bb53ee82d2a6f097ef628fb20055a503f
Adele Reed <adreed@microsoft.com>
2021-07-28 13:38:04 -0700

    add extra perms to blob SAS

--- azblob/sas_service.go
+++ azblob/sas_service.go
@@ -244,7 +244,10 @@ func (p *ContainerSASPermissions) Parse(s string) error {
 
 // The BlobSASPermissions type simplifies creating the permissions string for an Azure Storage blob SAS.
 // Initialize an instance of this type and then call its String method to set BlobSASSignatureValues's Permissions field.
-type BlobSASPermissions struct{ Read, Add, Create, Write, Delete, DeletePreviousVersion, Tag bool }
+type BlobSASPermissions struct{
+	Read, Add, Create, Write, Delete, DeletePreviousVersion, Tag bool
+	Execute, ModifyOwnership, ModifyPermissions bool // Hierarchical Namespace only
+}
 
 // String produces the SAS permissions string for an Azure Storage blob.
 // Call this method to set BlobSASSignatureValues's Permissions field.
@@ -271,6 +274,15 @@ func (p BlobSASPermissions) String() string {
 	if p.Tag {
 		b.WriteRune('t')
 	}
+	if p.Execute {
+		b.WriteRune('e')
+	}
+	if p.ModifyOwnership {
+		b.WriteRune('o')
+	}
+	if p.ModifyPermissions {
+		b.WriteRune('p')
+	}
 	return b.String()
 }
 
@@ -293,6 +305,12 @@ func (p *BlobSASPermissions) Parse(s string) error {
 			p.DeletePreviousVersion = true
 		case 't':
 			p.Tag = true
+		case 'e':
+			p.Execute = true
+		case 'o':
+			p.ModifyOwnership = true
+		case 'p':
+			p.ModifyPermissions = true
 		default:
 			return fmt.Errorf("invalid permission: '%v'", r)
 		}
