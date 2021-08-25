c8591edcdf868bf78abae0f09af37c6176330015
simin <siminsavani@microsoft.com>
2021-08-25 14:42:05 -0400

    Test for sdd

--- azblob/zt_user_delegation_sas_test.go
+++ azblob/zt_user_delegation_sas_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"github.com/Azure/azure-pipeline-go/pipeline"
 	chk "gopkg.in/check.v1"
+	"net/url"
 	"strings"
 	"time"
 )
@@ -297,4 +298,17 @@ func (s *aztestsSuite) TestUserDelegationCid(c *chk.C) {
 	bSASURL := NewBlockBlobURL(bSASParts.URL(), p)
 
 	c.Assert(strings.Contains(bSASURL.blobClient.url.RawQuery, "cid="+cid), chk.Equals, true)
+}
+
+func (s *aztestsSuite) TestParseSASQueryParams(c *chk.C) {
+	const blobURL = "https://myaccount.blob.core.windows.net/mycontainer/testfile?sp=r&st=2021-03-19T03:48:02Z&se=2021-03-19T11:48:02Z&spr=https&sv=2020-02-10&sr=d&sdd=10&sig=invalidsignature"
+
+	testURL, _ := url.Parse(blobURL)
+
+	bURLParts := NewBlobURLParts(*testURL)
+	sas := bURLParts.SAS
+
+	c.Assert(sas.resource, chk.Equals, "d")
+	c.Assert(sas.SignedDirectoryDepth(), chk.Equals, "10")
+	c.Assert(sas.protocol, chk.Equals, SASProtocolHTTPS)
 }
